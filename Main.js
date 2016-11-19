27e56b8e835673515a87c257b236569f44796781
Robert Hughes <hughesbkr@yahoo.com>
2016-11-19 14:42:28 -0700

    videos

--- Main.js
+++ Main.js
@@ -1,6 +1,8 @@
 angular.module("Hoppy", [])
-	.controller("HoppyController", function ($http)	{
-		var Hph=this;
-		Hph.users=[];
+	//build  a factory on aother page and just keep the above in here. 
+
+	//.controller("HoppyController", function ($http)	{
+	//	var Hph=this;
+	//	Hph.users=[];
 		
-	})
+	//})
